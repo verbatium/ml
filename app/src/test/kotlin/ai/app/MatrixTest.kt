39f4305d59e05b9254d02610d8d12ff05d737ff6
Valeri Kuzmin <valeri@codeborne.com>
2023-01-01 18:07:48 +0200

    refactoring use aliases in test

--- app/src/test/kotlin/ai/app/MatrixTest.kt
+++ app/src/test/kotlin/ai/app/MatrixTest.kt
@@ -1,141 +1,132 @@
 package ai.app
 
-import ai.app.Matrix.Companion.matrixOf
-import ai.app.Vector.Companion.vectorOf
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import java.math.MathContext
 import java.math.RoundingMode
+import ai.app.Matrix.Companion.matrixOf as m
+import ai.app.Vector.Companion.vectorOf as v
 
 class MatrixTest {
     @Test
-    fun determinant_1x1Matrix() {
-        val determinant = matrixOf(vectorOf(1)).determinant()
-        assertEquals(d(1), determinant)
-    }
+    fun determinant_1x1Matrix() = assertEquals(d(1), m(v(1)).determinant())
 
     @Test
-    fun determinant_2x2Matrix() {
-        val determinant = matrixOf(vectorOf(1, 2), vectorOf(2, 3)).determinant()
-        assertEquals(d(1 * 3 - 2 * 2), determinant)
-    }
+    fun determinant_2x2Matrix() = assertEquals(d(1 * 3 - 2 * 2), m(v(1, 2), v(2, 3)).determinant())
 
     @Test
-    fun determinant_3x3Matrix() {
-        val determinant = matrixOf(vectorOf(1, 2, 3), vectorOf(1, 1, 1), vectorOf(1, 2, 1)).determinant()
-        assertEquals(d(2), determinant)
-    }
+    fun determinant_3x3Matrix() = assertEquals(d(2), m(v(1, 2, 3), v(1, 1, 1), v(1, 2, 1)).determinant())
 
     @Test
     fun determinant_notSquareMatrix() {
-        assertThrows<IllegalArgumentException> { matrixOf(vectorOf(1, 2, 3), vectorOf(1, 1, 1)).determinant() }
-        assertThrows<IllegalArgumentException> { matrixOf(vectorOf(1, 2), vectorOf(1, 1), vectorOf(2, 2)).determinant() }
-        assertThrows<IllegalArgumentException> { matrixOf(vectorOf(1, 2), vectorOf(1)).determinant() }
+        assertThrows<IllegalArgumentException> { m(v(1, 2, 3), v(1, 1, 1)).determinant() }
+        assertThrows<IllegalArgumentException> { m(v(1, 2), v(1, 1), v(2, 2)).determinant() }
+        assertThrows<IllegalArgumentException> { m(v(1, 2), v(1)).determinant() }
     }
 
     @Test
     fun rank() {
-        assertEquals(1, matrixOf(vectorOf(1)).rank())
-        assertEquals(2, matrixOf(vectorOf(1, 2), vectorOf(3, 4)).rank())
+        assertEquals(1, m(v(1)).rank())
+        assertEquals(2, m(v(1, 2), v(3, 4)).rank())
         assertEquals(
-            2, matrixOf(
-                vectorOf(1, 2, 3),
-                vectorOf(4, 5, 6),
-                vectorOf(7, 8, 9)
+            2, m(
+                v(1, 2, 3),
+                v(4, 5, 6),
+                v(7, 8, 9)
             ).rank()
         )
         assertEquals(
-            3, matrixOf(
-                vectorOf(-1, -2, 3),
-                vectorOf(4, 5, 6),
-                vectorOf(7, 8, 9)
+            3, m(
+                v(-1, -2, 3),
+                v(4, 5, 6),
+                v(7, 8, 9)
             ).rank()
         )
         assertEquals(
-            3, matrixOf(
-                vectorOf(1, 2, 3, 4),
-                vectorOf(5, 6, 7, 8),
-                vectorOf(9, 0, 1, 2),
-                vectorOf(3, 4, 5, 6)
+            3, m(
+                v(1, 2, 3, 4),
+                v(5, 6, 7, 8),
+                v(9, 0, 1, 2),
+                v(3, 4, 5, 6)
             ).rank()
         )
 
         assertEquals(
-            2, matrixOf(
-                vectorOf(1, 1, 1),
-                vectorOf(1, 2, 0),
-                vectorOf(0, -1, 1),
-                vectorOf(3, 3, 3)
+            2, m(
+                v(1, 1, 1),
+                v(1, 2, 0),
+                v(0, -1, 1),
+                v(3, 3, 3)
             ).rank()
         )
     }
 
     @Test
     fun rank_zeroMatrix() {
-        assertEquals(0, matrixOf(vectorOf(0)).rank())
-        assertEquals(0, matrixOf(vectorOf(0), vectorOf(0)).rank())
-        assertEquals(0, matrixOf(vectorOf(0, 0)).rank())
-        assertEquals(0, matrixOf(vectorOf(0, 0), vectorOf(0, 0)).rank())
+        assertEquals(0, m(v(0)).rank())
+        assertEquals(0, m(v(0), v(0)).rank())
+        assertEquals(0, m(v(0, 0)).rank())
+        assertEquals(0, m(v(0, 0), v(0, 0)).rank())
     }
 
     @Test
     fun cofactor_excludeLastColumn() {
-        val matrix = matrixOf(
-            vectorOf(1, 2, 3),
-            vectorOf(3, 4, 3)
+        val matrix = m(
+            v(1, 2, 3),
+            v(3, 4, 3)
         )
-        assertEquals(matrixOf(vectorOf(1)), matrix.cofactor(listOf(1) to listOf(1, 2)))
-        assertEquals(matrixOf(vectorOf(2)), matrix.cofactor(listOf(1) to listOf(0, 2)))
-        assertEquals(matrixOf(vectorOf(3)), matrix.cofactor(listOf(0) to listOf(1, 2)))
-        assertEquals(matrixOf(vectorOf(4)), matrix.cofactor(listOf(0) to listOf(0, 2)))
+        assertEquals(m(v(1)), matrix.cofactor(listOf(1) to listOf(1, 2)))
+        assertEquals(m(v(2)), matrix.cofactor(listOf(1) to listOf(0, 2)))
+        assertEquals(m(v(3)), matrix.cofactor(listOf(0) to listOf(1, 2)))
+        assertEquals(m(v(4)), matrix.cofactor(listOf(0) to listOf(0, 2)))
     }
 
     @Test
     fun listOfCofactors() {
-        val matrix = matrixOf(
-            vectorOf(1, 2, 3),
-            vectorOf(4, 5, 6),
-            vectorOf(7, 8, 9)
+        val matrix = m(
+            v(1, 2, 3),
+            v(4, 5, 6),
+            v(7, 8, 9)
         )
         assertEquals(
             listOf(
-                matrixOf(
-                    vectorOf(5, 6),
-                    vectorOf(8, 9)
+                m(
+                    v(5, 6),
+                    v(8, 9)
                 ),
-                matrixOf(
-                    vectorOf(4, 6),
-                    vectorOf(7, 9)
+                m(
+                    v(4, 6),
+                    v(7, 9)
                 ),
-                matrixOf(
-                    vectorOf(4, 5),
-                    vectorOf(7, 8)
+                m(
+                    v(4, 5),
+                    v(7, 8)
                 ),
-                matrixOf(
-                    vectorOf(2, 3),
-                    vectorOf(8, 9)
+                m(
+                    v(2, 3),
+                    v(8, 9)
                 ),
-                matrixOf(
-                    vectorOf(1, 3),
-                    vectorOf(7, 9)
+                m(
+                    v(1, 3),
+                    v(7, 9)
                 ),
-                matrixOf(
-                    vectorOf(1, 2),
-                    vectorOf(7, 8)
+                m(
+                    v(1, 2),
+                    v(7, 8)
                 ),
-                matrixOf(
-                    vectorOf(2, 3),
-                    vectorOf(5, 6)
+                m(
+                    v(2, 3),
+                    v(5, 6)
                 ),
-                matrixOf(
-                    vectorOf(1, 3),
-                    vectorOf(4, 6)
+                m(
+                    v(1, 3),
+                    v(4, 6)
                 ),
-                matrixOf(
-                    vectorOf(1, 2),
-                    vectorOf(4, 5)
+                m(
+                    v(1, 2),
+                    v(4, 5)
                 ),
             ), matrix.cofactors(2 to 2)
         )
@@ -143,118 +134,116 @@ class MatrixTest {
 
     @Test
     fun transpose_square() {
-        val matrix = matrixOf(
-            vectorOf(1, 2),
-            vectorOf(3, 4)
+        val matrix = m(
+            v(1, 2),
+            v(3, 4)
         )
         val result = matrix.transpose()
-        val expected = matrixOf(
-            vectorOf(1, 3),
-            vectorOf(2, 4)
+        val expected = m(
+            v(1, 3),
+            v(2, 4)
         )
         assertEquals(expected, result)
     }
 
     @Test
-    fun transpose_horizontal() {
-        assertEquals(matrixOf(vectorOf(1),vectorOf( 2),vectorOf(3)), matrixOf(vectorOf(1, 2,3)).transpose())
-    }
+    fun transpose_horizontal() = assertEquals(m(v(1), v(2), v(3)), m(v(1, 2, 3)).transpose())
 
     @Test
     fun columns() {
-        assertEquals(listOf(vectorOf(1,3),vectorOf(2,4 )), matrixOf(vectorOf(1, 2), vectorOf(3,4)).columns())
-        assertEquals(listOf(vectorOf(1),vectorOf( 2),vectorOf(3)), matrixOf(vectorOf(1, 2,3)).columns())
+        assertEquals(listOf(v(1, 3), v(2, 4)), m(v(1, 2), v(3, 4)).columns())
+        assertEquals(listOf(v(1), v(2), v(3)), m(v(1, 2, 3)).columns())
     }
 
     @Test
     fun multiplyByScalar() {
-        val matrix = matrixOf(
-            vectorOf(1, 2),
-            vectorOf(3, 4)
+        val matrix = m(
+            v(1, 2),
+            v(3, 4)
         )
         val result = matrix * d(2)
-        val expected = matrixOf(
-            vectorOf(2, 4),
-            vectorOf(6, 8)
+        val expected = m(
+            v(2, 4),
+            v(6, 8)
         )
         assertEquals(expected, result)
     }
 
     @Test
     fun add() {
-        val a = matrixOf(
-            vectorOf(4, 2),
-            vectorOf(9, 0)
+        val a = m(
+            v(4, 2),
+            v(9, 0)
         )
-        val b = matrixOf(
-            vectorOf(3, 1),
-            vectorOf(-3, 4)
+        val b = m(
+            v(3, 1),
+            v(-3, 4)
         )
         val result = a + b
-        val expected = matrixOf(
-            vectorOf(7, 3),
-            vectorOf(6, 4)
+        val expected = m(
+            v(7, 3),
+            v(6, 4)
         )
         assertEquals(expected, result)
     }
 
     @Test
     fun minus() {
-        val a = matrixOf(
-            vectorOf(4, 2),
-            vectorOf(9, 0)
+        val a = m(
+            v(4, 2),
+            v(9, 0)
         )
-        val b = matrixOf(
-            vectorOf(3, 1),
-            vectorOf(-3, 4)
+        val b = m(
+            v(3, 1),
+            v(-3, 4)
         )
         val result = a - b
-        val expected = matrixOf(
-            vectorOf(1, 1),
-            vectorOf(12, -4)
+        val expected = m(
+            v(1, 1),
+            v(12, -4)
         )
         assertEquals(expected, result)
     }
 
     @Test
     fun multiplyMatrix() {
-        val a = matrixOf(
-            vectorOf(4, 2),
-            vectorOf(9, 0)
+        val a = m(
+            v(4, 2),
+            v(9, 0)
         )
-        val b = matrixOf(
-            vectorOf(3, 1),
-            vectorOf(-3, 4)
+        val b = m(
+            v(3, 1),
+            v(-3, 4)
         )
         val result = a * b
-        val expected = matrixOf(
-            vectorOf(6, 12),
-            vectorOf(27, 9)
+        val expected = m(
+            v(6, 12),
+            v(27, 9)
         )
         assertEquals(expected, result)
     }
 
     @Test
     fun minor() {
-        val matrix = matrixOf(
-            vectorOf(1, 2, 4),
-            vectorOf(2, 3, 5),
-            vectorOf(6, 7, 8),
+        val matrix = m(
+            v(1, 2, 4),
+            v(2, 3, 5),
+            v(6, 7, 8),
         )
         assertEquals(d(-1), matrix.minor(2, 2))
     }
 
     @Test
     fun adjugate() {
-        val matrix = matrixOf(
-            vectorOf(2, 4, 1),
-            vectorOf(0, 2, 1),
-            vectorOf(2, 1, 1),
+        val matrix = m(
+            v(2, 4, 1),
+            v(0, 2, 1),
+            v(2, 1, 1),
         )
-        val expected = matrixOf(
-            vectorOf(1, 2, -4),
-            vectorOf(-3, 0, 6),
-            vectorOf(2, -2, 4),
+        val expected = m(
+            v(1, 2, -4),
+            v(-3, 0, 6),
+            v(2, -2, 4),
         )
         val result = matrix.adjugate()
         assertEquals(expected, result)
@@ -263,15 +252,15 @@ class MatrixTest {
     @Test
     fun invertibleMatrix() {
         val mathContext = MathContext(2, RoundingMode.HALF_UP)
-        val matrix = matrixOf(
-            vectorOf(2, 4, 1),
-            vectorOf(0, 2, 1),
-            vectorOf(2, 1, 1),
+        val matrix = m(
+            v(2, 4, 1),
+            v(0, 2, 1),
+            v(2, 1, 1),
         )
-        val expected = matrixOf(
-            vectorOf(d(1).divide(d(6), mathContext), d(-0.5), d(1.0).divide(d(3), mathContext)),
-            vectorOf(d(1.0).divide(d(3), mathContext), d(0), d(-1.0).divide(d(3), mathContext)),
-            vectorOf(d(-2.0).divide(d(3), mathContext), d(1), d(2.0).divide(d(3), mathContext)),
+        val expected = m(
+            v(d(1).divide(d(6), mathContext), d(-0.5), d(1.0).divide(d(3), mathContext)),
+            v(d(1.0).divide(d(3), mathContext), d(0), d(-1.0).divide(d(3), mathContext)),
+            v(d(-2.0).divide(d(3), mathContext), d(1), d(2.0).divide(d(3), mathContext)),
         )
         val result = matrix.inverse(mathContext)
         assertEquals(expected, result)
@@ -279,42 +268,32 @@ class MatrixTest {
 
     @Test
     fun cofactor() {
-        val matrix = matrixOf(
-            vectorOf(1, 2, 4),
-            vectorOf(2, 3, 5),
-            vectorOf(6, 7, 8),
+        val matrix = m(
+            v(1, 2, 4),
+            v(2, 3, 5),
+            v(6, 7, 8),
         )
-        assertEquals(matrixOf(vectorOf(1, 2), vectorOf(2, 3)), matrix.cofactor(2, 2))
+        assertEquals(m(v(1, 2), v(2, 3)), matrix.cofactor(2, 2))
     }
 
     @Test
-    fun diagonalMatrix_check() {
-        assertTrue(matrixOf(vectorOf(1, 0), vectorOf(0, 1)).isDiagonal())
-    }
+    fun diagonalMatrix_check() = assertTrue(m(v(1, 0), v(0, 1)).isDiagonal())
 
     @Test
-    fun diagonalMatrix() {
-        assertEquals(matrixOf(vectorOf(1, 0), vectorOf(0, 2)), Matrix.diagonal(vectorOf(1, 2)))
-    }
+    fun diagonalMatrix() = assertEquals(m(v(1, 0), v(0, 2)), Matrix.diagonal(v(1, 2)))
 
     @Test
-    fun identity() {
-        assertEquals(matrixOf(vectorOf(1, 0), vectorOf(0, 1)), Matrix.identity(2))
-    }
+    fun identity() = assertEquals(m(v(1, 0), v(0, 1)), Matrix.identity(2))
 
     @Test
     fun scalar() {
-        assertEquals(matrixOf(vectorOf(3, 0), vectorOf(0, 3)), Matrix.scalar(3, 2))
-        assertEquals(matrixOf(vectorOf(2, 0), vectorOf(0, 2)), Matrix.scalar(d(2), 2))
+        assertEquals(m(v(3, 0), v(0, 3)), Matrix.scalar(3, 2))
+        assertEquals(m(v(2, 0), v(0, 2)), Matrix.scalar(d(2), 2))
     }
 
     @Test
-    fun vectorRow() {
-        assertEquals(matrixOf(vectorOf(1, 2,3)), Matrix.vectorRow(vectorOf(1, 2,3)))
-    }
+    fun vectorRow() = assertEquals(m(v(1, 2, 3)), Matrix.vectorRow(v(1, 2, 3)))
 
     @Test
-    fun vectorColumn() {
-        assertEquals(matrixOf(vectorOf(1),vectorOf( 2),vectorOf(3)), Matrix.vectorColumn(vectorOf(1, 2,3)))
-    }
+    fun vectorColumn() = assertEquals(m(v(1), v(2), v(3)), Matrix.vectorColumn(v(1, 2, 3)))
 }
