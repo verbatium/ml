e20bdffc572a0d64fdbed64f57dca6bc4defb5a3
Valeri Kuzmin <valeri@codeborne.com>
2023-01-10 22:10:47 +0200

    add Manhattan implementation of DistanceAlgorithm

---
+++ app/src/main/kotlin/ai/app/Manhattan.kt
@@ -0,0 +1,7 @@
+package ai.app
+
+import java.math.BigDecimal
+
+class Manhattan: DistanceAlgorithm {
+    override fun distance(values: Pair<Vector, Vector>): BigDecimal = (values.first - values.second).map { it.abs() }.sumOf { it }
+}
---
+++ app/src/test/kotlin/ai/app/ManhattanTest.kt
@@ -0,0 +1,15 @@
+package ai.app
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import java.math.BigDecimal
+
+class ManhattanTest {
+
+    @Test
+    fun distance() {
+        assertEquals(BigDecimal.valueOf(7), Manhattan().distance(Vector.vectorOf(1, 2) to Vector.vectorOf(4, 6)))
+        assertEquals(BigDecimal.valueOf(7), Manhattan().distance(Vector.vectorOf(4, 6) to Vector.vectorOf(1, 2)))
+    }
+}
