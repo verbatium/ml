47b368e8d314fa92110ea6baf8c5ad6a96bd4e67
Valeri Kuzmin <valeri@codeborne.com>
2023-01-12 18:38:33 +0200

    create skeleton for demo app

---
+++ demo/src/jvmMain/kotlin/Main.kt
@@ -0,0 +1,42 @@
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.withSave
+import androidx.compose.ui.res.loadImageBitmap
+import androidx.compose.ui.res.useResource
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+@Composable
+@Preview
+fun App() {
+    var buttonLabel by remember { mutableStateOf("Hello, World!") }
+    val sample = remember { useResource("sample.png", ::loadImageBitmap) }
+    MaterialTheme {
+        Canvas(modifier = Modifier.fillMaxSize()) {
+            drawIntoCanvas { canvas ->
+                canvas.withSave {
+                    canvas.drawImage(sample, Offset.Zero, Paint())
+                    canvas.translate(sample.width.toFloat(), 0f)
+                }
+            }
+        }
+        Button(onClick = { buttonLabel = "Hello, Desktop!" }) {
+            Text(buttonLabel)
+        }
+    }
+}
+
+fun main() = application {
+    Window(onCloseRequest = ::exitApplication) {
+        App()
+    }
+}
