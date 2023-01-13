c88f39abbae9fabac56edd8152c99e03ef2ee8f4
Valeri Kuzmin <valeri@codeborne.com>
2023-01-13 09:19:23 +0200

    draw xAxis on canvas

--- demo/src/jvmMain/kotlin/Main.kt
+++ demo/src/jvmMain/kotlin/Main.kt
@@ -8,31 +8,32 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
-import androidx.compose.ui.graphics.withSave
+import androidx.compose.ui.graphics.drawscope.translate
+import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.res.loadImageBitmap
 import androidx.compose.ui.res.useResource
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import org.jetbrains.skia.Font
+import java.math.BigDecimal
+import java.math.BigDecimal.ZERO
 
 @Composable
 @Preview
 fun App() {
     var buttonLabel by remember { mutableStateOf("Hello, World!") }
-    val sample = remember { useResource("sample.png", ::loadImageBitmap) }
+    val xAxis = Axis(ZERO, BigDecimal.TEN, 10)
     MaterialTheme {
         Column {
             Button(onClick = { buttonLabel = "Hello, Desktop!" }) {
                 Text(buttonLabel)
             }
             Canvas(modifier = Modifier.fillMaxSize()) {
-                drawIntoCanvas { canvas ->
-                    canvas.withSave {
-                        canvas.drawImage(sample, Offset.Zero, Paint())
-                        canvas.translate(sample.width.toFloat(), 0f)
-                    }
-                }
+                drawRect(color = Color.Gray)
+                xAxis.draw(this)
             }
         }
     }
@@ -41,5 +42,41 @@ fun App() {
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
         App()
+    }
+}
+
+data class Axis(
+    private val min: BigDecimal,
+    private val max: BigDecimal,
+    private val ticks: Int
+) {
+    private val textPaint = org.jetbrains.skia.Paint().apply {
+        isAntiAlias = true
+        color = org.jetbrains.skia.Color.BLUE
+    }
+
+    private val font = Font().apply {
+        size = 50f
+    }
+
+    private val width by lazy { (max - min).toFloat() }
+    fun draw(scope: DrawScope) {
+        val k = scope.size.width / width
+        scope.translate(0f, scope.size.height / 2f) {
+            this.drawIntoCanvas { c -> c.nativeCanvas.drawString(s = "0", x = 0f, y = -10f, font, paint = textPaint) }
+            this.drawLine(
+                color = Color.Black,
+                start = Offset(0f, 0f),
+                end = Offset(scope.size.width, 0f),
+            )
+            IntRange(0, ticks)
+                .map { it.toFloat() * k }.forEach {
+                    this.drawLine(
+                        color = Color.Black,
+                        start = Offset(it, -1f),
+                        end = Offset(it, 15f),
+                    )
+                }
+        }
     }
 }
